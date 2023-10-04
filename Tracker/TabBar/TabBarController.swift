0ca725a593c7d8b80dead82bf5ab58047c486c92
ArtemDubovitskiy <artemdubovicki@yandex.ru>
2023-10-04 22:05:02 +0300

    Добавлены иконки, структуры, главный экран "Trackers", TabBarController

---
+++ Tracker/TabBar/TabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarController.swift
+//  Tracker
+//
+//  Created by Artem Dubovitsky on 26.09.2023.
+//
+
+import Foundation
---
+++ Tracker/Trackers/TrackerStructures.swift
@@ -0,0 +1,8 @@
+//
+//  TrackerStructures.swift
+//  Tracker
+//
+//  Created by Artem Dubovitsky on 26.09.2023.
+//
+
+import Foundation
