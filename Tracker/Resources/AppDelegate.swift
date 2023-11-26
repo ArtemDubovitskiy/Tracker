09f5e9071e6bd6e9d341fc687ac5d3aa9cae7485
ArtemDubovitskiy <artemdubovicki@yandex.ru>
2023-11-27 01:46:31 +0300

    Добавлена инициализация CoreData и метод сохранения контекста в AppDelegate

--- Tracker/Resources/AppDelegate.swift
+++ Tracker/Resources/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Artem Dubovitsky on 05.09.2023.
 //
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -24,5 +25,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TrackerModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // TODO: Заменить на реализацию кодом обработку ошибки до отправки на ревью:
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+            
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                // TODO: Заменить на реализацию кодом обработку ошибки до отправки на ревью:
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 }
