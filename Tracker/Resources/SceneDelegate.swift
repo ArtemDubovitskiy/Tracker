ea49854e6afe165b9988c6774945de551c8c21fa
ArtemDubovitskiy <artemdubovicki@yandex.ru>
2023-12-28 15:02:51 +0300

    Добавлены экраны онбординга с использованием UIPageViewController.

--- Tracker/Resources/SceneDelegate.swift
+++ Tracker/Resources/SceneDelegate.swift
@@ -15,7 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = TabBarController()
+        window?.rootViewController = OnboardingPageViewController()
+//        window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
     }
 
---
+++ Tracker/Trackers/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,61 @@
+//
+//  OnboardingViewController.swift
+//  Tracker
+//
+//  Created by Artem Dubovitsky on 25.12.2023.
+//
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+    
+    private let onboardingImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let onboardingLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.textColor = .ypBlackDay
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    // MARK: - Initializers
+    init(onboardingImageName: String, onboardingText: String) {
+        super.init(nibName: nil, bundle: nil)
+        onboardingImageView.image = UIImage(named: onboardingImageName)
+        onboardingLabel.text = onboardingText
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupOnboardingView()
+        setupOnboardingViewConstrains()
+    }
+    // MARK: - Setup View
+    private func setupOnboardingView() {
+        view.addSubview(onboardingImageView)
+        view.addSubview(onboardingLabel)
+    }
+    
+    private func setupOnboardingViewConstrains() {
+        NSLayoutConstraint.activate([
+            onboardingImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            onboardingImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            onboardingImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            onboardingImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            onboardingLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            onboardingLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            onboardingLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -270)
+        ])
+    }
+}
